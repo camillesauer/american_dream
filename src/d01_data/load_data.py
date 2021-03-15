12fc89811067f1a7e13b03562b94b58806913c3d
camillesauer <camille-sauer@hotmail.fr>
2021-03-15 16:55:53 +0100

    fin de load

--- src/d01_data/load_data.py
+++ src/d01_data/load_data.py
@@ -12,9 +12,11 @@ mydb = mysql.connector.connect(
   password="PanapoiC19!"
 )
 
+#je me connecte à ma première db
 engine = sqlalchemy.create_engine("mysql+pymysql://student:PanapoiC19!@localhost/american_dream_db")
 
-#Je créer la db dans laquelle je vais stocker mon dataframe
+
+#Je crée la db dans laquelle je vais stocker mon dataframe
 # mycursor = mydb.cursor()
 # mycursor.execute("CREATE DATABASE american_dream_db")
 
@@ -23,17 +25,11 @@ data_analyst_db = pd.read_csv("/home/apprenant/PycharmProjects/american_dream/da
 salary_survey_db = pd.read_excel("/home/apprenant/PycharmProjects/american_dream/data/2020_Data_Professional_Salary_Survey_Responses.xlsx", engine='openpyxl', skiprows=3)
 
 #j'affiche les colonnes de mon premier fichier
-print(data_analyst_db.columns)
+# print(data_analyst_db.columns)
 #j'affiche les colonnes de mon second fichier
-print(salary_survey_db.columns)
+# print(salary_survey_db.columns)
 
-# j'insère la data du fichier csv dans la db sql
-# data_analyst_db.to_sql('Job Title', engine, if_exists='replace', index=False)
-# data_analyst_db.to_sql('Location', engine, if_exists='replace', index=False)
-# data_analyst_db.to_sql('Company Name', engine, if_exists='replace', index=False)
-# data_analyst_db.to_sql('Size', engine, if_exists='replace', index=False)
-# data_analyst_db.to_sql('Revenue', engine, if_exists='replace', index=False)
-# data_analyst_db.to_sql('Sector', engine, if_exists='replace', index=False)
-# data_analyst_db.to_sql('Salary Estimate', engine, if_exists='replace', index=False)
-# data_analyst_db.to_sql('Type of ownership', engine, if_exists='replace', index=False)
+# j'insère la data du fichier csv et excel dans la db sql et crée deux tables distinctes
+salary_survey_db.to_sql('SalarySurvey', engine, if_exists='replace', index=False)
+data_analyst_db.to_sql('DataAnalyst', engine, if_exists='replace', index=False)
 
--- src/d02_intermediate/create_int_payment_data.py
+++ src/d02_intermediate/create_int_payment_data.py
@@ -2,9 +2,19 @@
 from src.d01_data.load_data import *
 
 #j'affiche les valeurs d'une colonne
-test = salary_survey_db.unique['CompanyEmployeesOverall']
-print(test)
+# test = salary_survey_db.unique['CompanyEmployeesOverall']
+# print(test)
 
-#j'affiche les valeurs d'une colonne
-#df_1 = salary_survey_db.loc.head[:,'EmploymentStatus']
-#print(df_1)
+
+engine = sqlalchemy.create_engine("mysql+pymysql://student:PanapoiC19!@localhost/american_dream_db")
+df = pd.read_sql('SELECT * FROM DataAnalyst', con=engine)
+print(df.columns)
+
+df1 = pd.read_sql('SELECT * FROM SalarySurvey', con=engine)
+print(df1.columns)
+
+#Je supprime les colonnes dont je n'ai pas besoin dans les tables
+# mycursor = mydb.cursor()
+# mycursor.execute("ALTER TABLE "DataAnalyst" DROP "column_name"; american_dream_db")
+
+# mycursor.execute("ALTER TABLE "SalarySurvey" DROP "column_name"; american_dream_db")
